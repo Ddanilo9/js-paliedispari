bf239abdce02e31d734f8d5690fee6eccfb24ce0
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-28 18:17:42 +0200

    fix esercizio 1

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
  console.log('hola')
-// // let parola = 'anna';
+// let parola = 'anna';
 
-// // let parolaDivisa = parola.split('');
-// // console.log(parolaDivisa);
+// let parolaDivisa = parola.split('');
+// console.log(parolaDivisa);
 
-// // parolaReverse = parolaDivisa.reverse();
-// // console.log(parolaDivisa);
+// parolaReverse = parolaDivisa.reverse();
+// console.log(parolaDivisa);
 
-// // let parolaInvertita = parolaDivisa.join('');
-// // console.log(parolaInvertita);
+// let parolaInvertita = parolaDivisa.join('');
+// console.log(parolaInvertita);
 
-// // if(parola == parolaInvertita){
-// //     console.log('la parola è palindroma');
-// //   } else {
-// //     console.log('la parola non è palindroma');
-// //   }
-  
-
-
-// let parola = 'anna';  //mi creo una variabile con un palindromo
-
-// let parolaInversa = invertiParola(parola);   //attivo la funzione su questa variabile
-
-// //scrivo la condizione per vedere se la parola inversa è palindroma
-// if(parola == parolaInversa){
+// if(parola == parolaInvertita){
 //     console.log('la parola è palindroma');
 //   } else {
 //     console.log('la parola non è palindroma');
 //   }
+  
 
-//   //creo la funzione
-// function invertiParola(stringa){
-//   let stringaInversa = stringa.split('').reverse().join('');  
-//   return stringaInversa;
-// }
+
+let parola = 'anna';  //mi creo una variabile con un palindromo
+
+let parolaInversa = invertiParola(parola);   //attivo la funzione su questa variabile
+
+//scrivo la condizione per vedere se la parola inversa è palindroma
+if(parola == parolaInversa){
+    console.log('la parola è palindroma');
+  } else {
+    console.log('la parola non è palindroma');
+  }
+
+  //creo la funzione
+function invertiParola(stringa){
+  let stringaInversa = stringa.split('').reverse().join('');  
+  return stringaInversa;
+}
 
 //********************************************************************************************************************* */
 
